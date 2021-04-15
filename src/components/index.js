4ee3a632f4d7c62f714c015790a1e011f34beff5
Anderson Almeida <andi.r.almeida@gmail.com>
2021-04-14 22:24:55 -0300

    update component lib structure

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+/**
+ * Elements
+ */
+export { default as Button } from "./elements/Button";
+
+/**
+ * Patterns
+ */
+export { default as MyPattern } from "./patterns/MyPattern";
+
+/**
+ * Templates
+ */
+export { default as HelloWorld } from "./templates/HelloWorld";
---
+++ src/library.js
@@ -0,0 +1,23 @@
+import * as components from "./components";
+
+const ComponentLibary = {
+  install(Vue, options) {
+    if (options) {
+      // adiciona globalmente a configuração
+      Vue.prototype.$compLib = options;
+    }
+
+    // components
+    for (const componentName in components) {
+      const component = components[componentName];
+
+      Vue.component(component.name, component);
+    }
+  },
+};
+
+export default ComponentLibary;
+
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.use(ComponentLibary);
+}
